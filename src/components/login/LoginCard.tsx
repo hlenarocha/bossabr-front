656fbdf6028924b36d4c4deeeffb00490481eca5
hlenarocha <helenavitoria1200@gmail.com>
2025-01-21 11:24:36 -0300

    [style]: add login screen responsiveness

--- src/components/login/LoginCard.tsx
+++ src/components/login/LoginCard.tsx
@@ -5,25 +5,36 @@ import LoginInput from "./LoginInput";
 import LoginButton from "./LoginButton";
 
 const LoginCard: React.FC = () => {
-    return (
-        <div className="items-center absolute justify-center w-[600px] flex flex-col h-[600px] bg-black/10 rounded-full shadow-[inset_-4px_-4px_5px_0px_rgba(255,255,255,0.25),inset_4px_4px_5px_0px_rgba(255,255,255,0.25)] backdrop-blur-[30px]" >
-            <img className="mx-auto mt-[-20px]" src={Logo} alt="Logo Bossa"/>
-            <div className="w-[368px] h-[46px] text-white text-[32px] font-normal font-alatsi " >Login</div>
-            <LoginInput placeholder="E-mail"></LoginInput>
-            <LoginInput placeholder="Senha"></LoginInput>
-            <div className="w-[368px] mt-[10px] h-[19px] text-right text-white text-base font-normal font-alatsi underline hover:cursor-pointer hover:opacity-70">Esqueci minha senha!</div>
-            <LoginButton></LoginButton>
-            <div className="w-[371px] mt-[20px] h-[1px] bg-white"></div>
-            <div className="w-[371px] h-[38px] hover:border-[2px] hover:border-[#F23768] hover:cursor-pointer flex items-center justify-center bg-[#ffffff] rounded-[10px] shadow-[28px_28px_50px_0px_rgba(17,17,17,0.25)] mt-5 placeholder-gray-500 transform hover:scale-100">
-                <div className="bg-white w-[30px] h-[30px] rounded-full flex items-center justify-center mr-[10px]">
-                    <img src={GoogleIcon} alt="Google"/>
-                </div>
-                <div className="text-[#000000] font-alatsi font-[500]">
-                    Continuar com o Google
-                </div>
-            </div>
+  return (
+    /* Responsive Design in Tailwind occcurs through Breakpoints, which are defined in the tailwind.config.js file:
+        sm (small): minimum width of 640px
+        md (medium): minimum width of 768px
+        lg (large): minimum width of 1024px
+        xl (extra large): minimum width of 1280px
+        2xl (2x extra large): minimum width of 1536px
+    */
+    <div className="w-[90%] max-w-[600px] flex absolute flex-col justify-center items-center h-auto sm:h-[600px] bg-black/10 rounded-lg sm:rounded-full p-10 shadow-[inset_-4px_-4px_5px_0px_rgba(255,255,255,0.25),inset_4px_4px_5px_0px_rgba(255,255,255,0.25)] backdrop-blur-[30px]">
+      <img className="mx-auto mt-[-20px]" src={Logo} alt="Logo Bossa" />
+      <div className="w-full max-w-[368px] h-[46px] text-white text-[32px] font-normal font-alatsi text-left">
+        Login
+      </div>
+        <LoginInput placeholder="E-mail"></LoginInput>
+        <LoginInput placeholder="Senha"></LoginInput>
+      <div className="w-full max-w-[368px] mt-[10px] h-[19px] text-right text-white text-base font-normal font-alatsi underline hover:cursor-pointer hover:opacity-70">
+        Esqueci minha senha!
+      </div>
+      <LoginButton></LoginButton>
+      <div className="w-full max-w-[371px] mt-[20px] h-[1px] bg-white"></div>
+      <div className="w-full max-w-[371px] h-[38px] hover:border-[2px] hover:border-[#F23768] hover:cursor-pointer flex items-center justify-center bg-[#ffffff] rounded-[10px] shadow-[28px_28px_50px_0px_rgba(17,17,17,0.25)] mt-5 placeholder-gray-500 transform hover:scale-100">
+        <div className="bg-white w-[30px] h-[30px] rounded-full flex items-center justify-center mr-[10px]">
+          <img src={GoogleIcon} alt="Google" />
         </div>
-    )
+        <div className="text-[#000000] font-alatsi font-[500]">
+          Continuar com o Google
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginCard;
--- src/components/login/LoginInput.tsx
+++ src/components/login/LoginInput.tsx
@@ -6,7 +6,7 @@ interface LoginInputProps {
 const LoginInput = (prop: LoginInputProps) => {
     return (
         <>
-            <input className="w-[371px] h-[38px] bg-[#fffffe] rounded-[10px] shadow-[28px_28px_50px_0px_rgba(17,17,17,0.25)] shadow-[inset_-31px_4px_43px_0px_rgba(228,228,228,0.64)] p-4 mt-5 placeholder-gray-500" placeholder={prop.placeholder}></input>
+            <input className="w-full max-w-[371px] h-[38px] bg-[#fffffe] rounded-[10px] shadow-[28px_28px_50px_0px_rgba(17,17,17,0.25)] p-4 mt-5 placeholder-gray-500" placeholder={prop.placeholder}></input>
         </>
     );
 }
