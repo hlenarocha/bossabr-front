d131461536473208e559369a2ef10a61a25d6322
Carolina Picheictt <picheictt.carolina@gmail.com>
2025-01-23 08:50:40 -0300

    Adição do Link CDN no html principal Adição de sugestões primárias e exemplos de aplicação de icons na BaseScreen.tsx

--- src/screens/BaseScreen.tsx
+++ src/screens/BaseScreen.tsx
@@ -6,13 +6,22 @@ const BaseScreen: React.FC = () => {
       <div>
       <div className="w-full text-[32px] text-center text-white">Aqui será a tela base</div>
       <div className="w-full text-[24px] text-center text-pink-600">Teste de rotas</div>
-
+          <div>
+              <p>Icons para facilitar a procura dos Icons :)</p>
+              <p><i className="fa-solid fa-chart-line"></i> - Dashboard</p>
+              <p><i className="fa-solid fa-file-lines"></i> - Relatórios</p>
+              <p><i className="fa-solid fa-desktop"></i> - Área de Trabalho</p>
+              <p><i className="fa-solid fa-user"></i> - Clientes</p>
+              <p><i className="fa-solid fa-list-check"></i> <i className="fa-solid fa-bars-progress"></i> - Demandas</p>
+              <p><i className="fa-solid fa-gear"></i> - Configurações</p>
+              <p>Site para encontrar mais icons (e melhores kkkk): https://fontawesome.com/search?ic=free</p>
+          </div>
       </div>
-      
+
 
     </div>
   );
-  
+
 }
 
 export default BaseScreen;
