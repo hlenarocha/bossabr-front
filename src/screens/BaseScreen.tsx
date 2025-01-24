45f46f576968b8aa0a6de767ceed029fd7a5e10c
hlenarocha <helenavitoria1200@gmail.com>
2025-01-24 11:46:17 -0300

    [test]: testing background image at BaseScreen

--- src/screens/BaseScreen.tsx
+++ src/screens/BaseScreen.tsx
@@ -1,27 +1,196 @@
 import React from "react";
+import BackgroundImage from "../assets/images/abstract-background.svg";
 
 const BaseScreen: React.FC = () => {
-  return(
-    <div className="h-screen flex-col w-full items-center justify-center flex bg-customGray">
-      <div>
-      <div className="w-full text-[32px] text-center text-white">Aqui será a tela base</div>
-      <div className="w-full text-[24px] text-center text-pink-600">Teste de rotas</div>
-          <div>
-              <p>Icons para facilitar a procura dos Icons :)</p>
-              <p><i className="fa-solid fa-chart-line"></i> - Dashboard</p>
-              <p><i className="fa-solid fa-file-lines"></i> - Relatórios</p>
-              <p><i className="fa-solid fa-desktop"></i> - Área de Trabalho</p>
-              <p><i className="fa-solid fa-user"></i> - Clientes</p>
-              <p><i className="fa-solid fa-list-check"></i> <i className="fa-solid fa-bars-progress"></i> - Demandas</p>
-              <p><i className="fa-solid fa-gear"></i> - Configurações</p>
-              <p>Site para encontrar mais icons (e melhores kkkk): https://fontawesome.com/search?ic=free</p>
-          </div>
+  return (
+    <div
+      className="bg-cover bg-center bg-no-repeat bg-fixed min-h-screen bg-[#bbbbbb]"
+      style={{ backgroundImage: `url(${BackgroundImage})` }} // Apenas a URL precisa ser aplicada inline
+    >
+      <div className="flex flex-col items-center justify-center min-h-screen text-center text-white">
+        <div className="text-[32px]">Aqui será a tela base</div>
+        <div className="text-[24px] text-pink-600">Teste de rotas</div>
+        <div className="mt-4">
+          <p>Icons para facilitar a procura dos Icons :)</p>
+          <p>
+            <i className="fa-solid fa-chart-line"></i> - Dashboard
+          </p>
+          <p>
+            <i className="fa-solid fa-file-lines"></i> - Relatórios
+          </p>
+          <p>
+            <i className="fa-solid fa-desktop"></i> - Área de Trabalho
+          </p>
+          <p>
+            <i className="fa-solid fa-user"></i> - Clientes
+          </p>
+          <p>
+            <i className="fa-solid fa-list-check"></i>{" "}
+            <i className="fa-solid fa-bars-progress"></i> - Demandas
+          </p>
+          <p>
+            <i className="fa-solid fa-gear"></i> - Configurações
+          </p>
+          <p>
+            Site para encontrar mais icons (e melhores kkkk):{" "}
+            <a href="https://fontawesome.com/search?ic=free" target="_blank" rel="noopener noreferrer">
+              https://fontawesome.com/search?ic=free
+            </a>
+          </p>
+        </div>
+        <div className="text-[32px]">Aqui será a tela base</div>
+        <div className="text-[24px] text-pink-600">Teste de rotas</div>
+        <div className="mt-4">
+          <p>Icons para facilitar a procura dos Icons :)</p>
+          <p>
+            <i className="fa-solid fa-chart-line"></i> - Dashboard
+          </p>
+          <p>
+            <i className="fa-solid fa-file-lines"></i> - Relatórios
+          </p>
+          <p>
+            <i className="fa-solid fa-desktop"></i> - Área de Trabalho
+          </p>
+          <p>
+            <i className="fa-solid fa-user"></i> - Clientes
+          </p>
+          <p>
+            <i className="fa-solid fa-list-check"></i>{" "}
+            <i className="fa-solid fa-bars-progress"></i> - Demandas
+          </p>
+          <p>
+            <i className="fa-solid fa-gear"></i> - Configurações
+          </p>
+          <p>
+            Site para encontrar mais icons (e melhores kkkk):{" "}
+            <a href="https://fontawesome.com/search?ic=free" target="_blank" rel="noopener noreferrer">
+              https://fontawesome.com/search?ic=free
+            </a>
+          </p>
+        </div>
+        <div className="text-[32px]">Aqui será a tela base</div>
+        <div className="text-[24px] text-pink-600">Teste de rotas</div>
+        <div className="mt-4">
+          <p>Icons para facilitar a procura dos Icons :)</p>
+          <p>
+            <i className="fa-solid fa-chart-line"></i> - Dashboard
+          </p>
+          <p>
+            <i className="fa-solid fa-file-lines"></i> - Relatórios
+          </p>
+          <p>
+            <i className="fa-solid fa-desktop"></i> - Área de Trabalho
+          </p>
+          <p>
+            <i className="fa-solid fa-user"></i> - Clientes
+          </p>
+          <p>
+            <i className="fa-solid fa-list-check"></i>{" "}
+            <i className="fa-solid fa-bars-progress"></i> - Demandas
+          </p>
+          <p>
+            <i className="fa-solid fa-gear"></i> - Configurações
+          </p>
+          <p>
+            Site para encontrar mais icons (e melhores kkkk):{" "}
+            <a href="https://fontawesome.com/search?ic=free" target="_blank" rel="noopener noreferrer">
+              https://fontawesome.com/search?ic=free
+            </a>
+          </p>
+        </div>
+        <div className="text-[32px]">Aqui será a tela base</div>
+        <div className="text-[24px] text-pink-600">Teste de rotas</div>
+        <div className="mt-4">
+          <p>Icons para facilitar a procura dos Icons :)</p>
+          <p>
+            <i className="fa-solid fa-chart-line"></i> - Dashboard
+          </p>
+          <p>
+            <i className="fa-solid fa-file-lines"></i> - Relatórios
+          </p>
+          <p>
+            <i className="fa-solid fa-desktop"></i> - Área de Trabalho
+          </p>
+          <p>
+            <i className="fa-solid fa-user"></i> - Clientes
+          </p>
+          <p>
+            <i className="fa-solid fa-list-check"></i>{" "}
+            <i className="fa-solid fa-bars-progress"></i> - Demandas
+          </p>
+          <p>
+            <i className="fa-solid fa-gear"></i> - Configurações
+          </p>
+          <p>
+            Site para encontrar mais icons (e melhores kkkk):{" "}
+            <a href="https://fontawesome.com/search?ic=free" target="_blank" rel="noopener noreferrer">
+              https://fontawesome.com/search?ic=free
+            </a>
+          </p>
+        </div>
+        <div className="text-[32px]">Aqui será a tela base</div>
+        <div className="text-[24px] text-pink-600">Teste de rotas</div>
+        <div className="mt-4">
+          <p>Icons para facilitar a procura dos Icons :)</p>
+          <p>
+            <i className="fa-solid fa-chart-line"></i> - Dashboard
+          </p>
+          <p>
+            <i className="fa-solid fa-file-lines"></i> - Relatórios
+          </p>
+          <p>
+            <i className="fa-solid fa-desktop"></i> - Área de Trabalho
+          </p>
+          <p>
+            <i className="fa-solid fa-user"></i> - Clientes
+          </p>
+          <p>
+            <i className="fa-solid fa-list-check"></i>{" "}
+            <i className="fa-solid fa-bars-progress"></i> - Demandas
+          </p>
+          <p>
+            <i className="fa-solid fa-gear"></i> - Configurações
+          </p>
+          <p>
+            Site para encontrar mais icons (e melhores kkkk):{" "}
+            <a href="https://fontawesome.com/search?ic=free" target="_blank" rel="noopener noreferrer">
+              https://fontawesome.com/search?ic=free
+            </a>
+          </p>
+        </div>
+        <div className="text-[32px]">Aqui será a tela base</div>
+        <div className="text-[24px] text-pink-600">Teste de rotas</div>
+        <div className="mt-4">
+          <p>Icons para facilitar a procura dos Icons :)</p>
+          <p>
+            <i className="fa-solid fa-chart-line"></i> - Dashboard
+          </p>
+          <p>
+            <i className="fa-solid fa-file-lines"></i> - Relatórios
+          </p>
+          <p>
+            <i className="fa-solid fa-desktop"></i> - Área de Trabalho
+          </p>
+          <p>
+            <i className="fa-solid fa-user"></i> - Clientes
+          </p>
+          <p>
+            <i className="fa-solid fa-list-check"></i>{" "}
+            <i className="fa-solid fa-bars-progress"></i> - Demandas
+          </p>
+          <p>
+            <i className="fa-solid fa-gear"></i> - Configurações
+          </p>
+          <p>
+            Site para encontrar mais icons (e melhores kkkk):{" "}
+            <a href="https://fontawesome.com/search?ic=free" target="_blank" rel="noopener noreferrer">
+              https://fontawesome.com/search?ic=free
+            </a>
+          </p>
+        </div>
       </div>
-
-
     </div>
   );
-
-}
+};
 
 export default BaseScreen;
