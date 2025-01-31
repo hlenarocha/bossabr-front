3de77f52b64574c9c67014014313234c8bd90a90
Carolina Picheictt <picheictt.carolina@gmail.com>
2025-01-31 16:32:25 -0300

    Espaçamento das letras do Date

--- src/components/header/HeaderDate.tsx
+++ src/components/header/HeaderDate.tsx
@@ -5,12 +5,12 @@ const HeaderDate = () => {
   const { day, monthName, dayOfWeek } = getDateInfo();
 
   return <>
-  <div className="flex justify-center items-center gap-2">
+  <div className="flex justify-center items-center gap-3">
     <div className="text-white text-[64px] font-black font-albert">
       {day}
 
     </div>
-    <div className="bg-white w-[4px] h-[60px]"></div>
+    <div className="bg-white w-[3px] h-[55px]"></div>
     <div className="flex flex-col items-center">
       <div className="text-white text-4xl font-black">{monthName}</div>
       <div className="text-white text-base font-bold">{dayOfWeek}</div>
