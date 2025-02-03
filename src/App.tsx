eec9a27c8a7b29fa6fac56f94110492ce0407f78
hlenarocha <helenavitoria1200@gmail.com>
2025-02-03 17:55:44 -0300

    [fix]: outline removed at LoginScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./screens/LoginScreen";
 import Base from "./screens/BaseScreen";
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <Router>
       <Routes>
--- src/components/login/LoginInput.tsx
+++ src/components/login/LoginInput.tsx
@@ -6,7 +6,7 @@ interface LoginInputProps {
 const LoginInput = (prop: LoginInputProps) => {
     return (
         <>
-            <input className="w-full max-w-[371px] h-[38px] bg-[#fffffe] rounded-[10px] shadow-[28px_28px_50px_0px_rgba(17,17,17,0.25)] p-4 mt-5 placeholder-gray-500" placeholder={prop.placeholder}></input>
+            <input className="w-full max-w-[371px] h-[38px] bg-[#fffffe] rounded-[10px] shadow-[28px_28px_50px_0px_rgba(17,17,17,0.25)] p-4 mt-5 placeholder-gray-500 outline-none focus:ring-2 focus:ring-[#F6BC0A]" placeholder={prop.placeholder}></input>
         </>
     );
 }
