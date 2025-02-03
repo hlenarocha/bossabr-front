044ac83abc60265772b1423a57ace44aae3e139c
hlenarocha <helenavitoria1200@gmail.com>
2025-02-03 20:58:42 -0300

    [feat]: add SideBarItems

--- src/components/sidebar/SideBarItems.tsx
+++ src/components/sidebar/SideBarItems.tsx
@@ -1,11 +1,16 @@
 
-/*interface SideBarItemsProps {
-------- EM OBRAS ------
-}*/
+interface SideBarItemsProps {
+  icon: string;
+  title: string;
+}
 
-const SideBarItems = () => {
+const SideBarItems = (prop: SideBarItemsProps) => {
   return (
     <>
+    <div className="text-white flex flex-row items-center p-5 w-full">
+      <i className={prop.icon} aria-hidden="true"></i>
+      <div className="ml-5">{prop.title}</div>
+    </div>
 
     </>
 
