1d542b81c5c29616392227c040d02b40f9de9ac7
hlenarocha <helenavitoria1200@gmail.com>
2025-02-26 12:08:04 -0300

    Refactor InputText component and update CreateWorker screen to use new InputText props

--- src/components/UI/InputText.tsx
+++ src/components/UI/InputText.tsx
@@ -11,8 +11,12 @@ interface InputTextProps {
 const InputText = (props: InputTextProps) => {
   return (
     <>
+    <div className={`flex flex-col mb-4 ${props.width}`}>
     <div className="text-sm mt-2 font-black mb-1 text-white">{props.title}<span className={` ${props.isMandatory ? "visible" : "hidden"} text-customYellow text-xl`}> *</span></div>
-    <input className={`${props.width} ${props.height} bg-customInputGray py-2 px-4 border outline-none border-customYellow rounded-[400px]`} placeholder={props.placeholder} type="text"></input>
+    <input className={` ${props.height} bg-customInputGray py-2 px-4 border outline-none border-customYellow rounded-[400px]`} placeholder={props.placeholder} type="text"></input>
+
+    </div>
+    
     </>
   );
 
--- src/screens/settings/CreateWorker.tsx
+++ src/screens/settings/CreateWorker.tsx
@@ -5,7 +5,7 @@ import Box from "../../components/box/BoxContent";
 import InputTitle from "../../components/title/InputTitle";
 import PageTitle from "../../components/title/PageTitle";
 import InputText from "../../components/UI/InputText";
-
+import ColoredButton from "../../components/UI/ColoredButton";
 
 const CreateWorker = () => {
   const navigate = useNavigate();
@@ -14,24 +14,72 @@ const CreateWorker = () => {
     navigate(path);
   }
 
-  return(
+  return (
     <>
-    <BaseScreen>
-    <BackButton onClick={() => handleNavigate("/settings/configure-worker")}>
-    </BackButton>
-    <PageTitle title="Cadastrar Colaborador"></PageTitle>
+      <BaseScreen>
+        <BackButton
+          onClick={() => handleNavigate("/settings/configure-worker")}
+        ></BackButton>
+        <PageTitle title="Cadastrar Colaborador"></PageTitle>
 
-    <Box title="Cadastrar" subtitle="Cadastre um colaborador aqui." width="w-[1030px]" height="h-[630px]">
-      <InputTitle title="Funcionário"></InputTitle>
-      <InputText title="NOME DO COLABORADOR" width="w-[100%]" height="h-8" placeholder="Digite o nome..." isMandatory={true}></InputText>
+        <Box
+          title="Cadastrar"
+          subtitle="Cadastre um colaborador aqui."
+          width="w-[1030px]"
+          height="h-[630px]"
+        >
+          <InputTitle title="Funcionário"></InputTitle>
+          <InputText
+            title="NOME DO COLABORADOR"
+            width="w-[100%]"
+            height="h-8"
+            placeholder="Digite o nome..."
+            isMandatory={true}
+          ></InputText>
 
-    </Box>
+          <div className="flex gap-4 flex-row justify-between items-center w-[100%]">
+            <InputText
+              title="E-MAIL"
+              width="w-[50%]"
+              height="h-8"
+              placeholder="Digite o email..."
+              isMandatory={true}
+            ></InputText>
+            <InputText
+              title="TELEFONE"
+              width="w-[50%]"
+              height="h-8"
+              placeholder="Digite o telefone..."
+              isMandatory={true}
+            ></InputText>
+          </div>
+
+          <div className="flex gap-4 flex-row justify-between items-center w-[100%]">
+            <InputText
+              title="DATA DE ANIVERSÁRIO"
+              width="w-[50%]"
+              height="h-8"
+              placeholder="Digite o email..."
+              isMandatory={false}
+            ></InputText>
+            <InputText
+              title="DATA DE ENTRADA"
+              width="w-[50%]"
+              height="h-8"
+              placeholder="Digite o telefone..."
+              isMandatory={false}
+            ></InputText>
+          </div>
+
+          <div className="w-[100%] flex justify-center mt-4">
+          <ColoredButton title="SALVAR" color="customYellow" width="w-[180px]" justify="justify-center" icon="fa-solid fa-floppy-disk" ></ColoredButton>
 
 
-    </BaseScreen>
+          </div>
+        </Box>
+      </BaseScreen>
     </>
   );
-
-}
+};
 
 export default CreateWorker;
