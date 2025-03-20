0b2c4d90703aafcc024831e454115986b31a5fbc
hlenarocha <helenavitoria1200@gmail.com>
2025-03-20 09:26:40 -0300

    feat: Update .env and .gitignore files, and modify LoginScreen.tsx to use environment variables

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -2,13 +2,16 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import Background3D from "../components/3D/Background3D";
 import LoginCard from "../components/login/LoginCard";
 
+
 const App = () => {
+  const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+  
   return (
     <div className="relative w-full h-screen flex justify-center items-center bg-customGray overflow-hidden">
       <Background3D />
 
     {/* Cliente id do google */}
-      <GoogleOAuthProvider clientId="1021397316105-8bhmiu5d8ela5dsaoomf7rgued8hc6do.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId={googleClientId}>
         <LoginCard />
       </GoogleOAuthProvider>
     </div>
