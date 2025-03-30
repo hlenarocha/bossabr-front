cbd159f91bff05056892c63a1dfb96a8295a0dab
hlenarocha <helenavitoria1200@gmail.com>
2025-03-30 18:42:44 -0300

    feat: updated login card component with modal error for status 403

--- src/components/login/LoginCard.tsx
+++ src/components/login/LoginCard.tsx
@@ -9,11 +9,14 @@ import LoginButton from "@/components/login/LoginButton";
 import Logo from "@/assets/images/logo-bossa.svg";
 import GoogleIcon from "@/assets/images/icon-google.png";
 import LoadingScreen from "@/screens/loading/LoadingScreen";
+import Modal from "@/components/modal/Modal";
+import IconSad from "@/assets/images/famicons_sad.png";
 
 const LoginCard = () => {
   const navigate = useNavigate();
   const { setUser } = useContext(UserContext);
   const [loading, setLoading] = useState(false);
+  const [isModalErrorVisible, setIsModalErrorVisible] = useState(false);
 
   const login = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
@@ -31,10 +34,9 @@ const LoginCard = () => {
         console.log(response?.status);
 
         if (response?.status === 403) {
-          alert("Usuário não autorizado");
+          setIsModalErrorVisible(true);
           return;
         }
-
 
         if (response?.token && response.user) {
           setLoading(true);
@@ -58,10 +60,10 @@ const LoginCard = () => {
 
           // 4. Redireciona e exibe tela de carregamento
           setTimeout(() => {
-          setLoading(false); // Reseta loading state
-          navigate("/workspace");
-        }, 2000);
-      }
+            setLoading(false); // Reseta loading state
+            navigate("/workspace");
+          }, 2000);
+        }
       } catch (error) {
         console.error("Erro no login:", error);
         // Mostrar feedback para o usuário (ex.: toast.error)
@@ -98,31 +100,42 @@ const LoginCard = () => {
       xl (extra large): minimum width of 1280px
       2xl (2x extra large): minimum width of 1536px
   */
+    <>
+      <Modal
+      title="Usuário não autorizado!"
+      description="Entre em contato com o administrador do sistema caso isso seja um erro."
+      onClick1={() => setIsModalErrorVisible(false)}
+      isModalVisible={isModalErrorVisible}
+      buttonTitle1="FECHAR"
+      iconImage={IconSad}
+      
+      ></Modal>
 
-    <div className="w-[90%] max-w-[600px] flex absolute flex-col justify-center items-center h-auto sm:h-[600px] bg-black/10 rounded-lg sm:rounded-full p-10 shadow-[inset_-4px_-4px_5px_0px_rgba(255,255,255,0.25),inset_4px_4px_5px_0px_rgba(255,255,255,0.25)] backdrop-blur-[30px]">
-      <img className="mx-auto mt-[-20px]" src={Logo} alt="Logo Bossa" />
-      <div className="w-full cursor-default max-w-[368px] h-[46px] text-white text-[32px] font-normal font-alatsi text-left">
-        Login
-      </div>
-      <LoginInput placeholder="E-mail"></LoginInput>
-      <LoginInput placeholder="Senha"></LoginInput>
-      <div className="w-full max-w-[368px] mt-[10px] h-[19px] text-right text-white text-base font-normal font-alatsi underline hover:cursor-pointer hover:opacity-70">
-        Esqueci minha senha!
-      </div>
-      <LoginButton></LoginButton>
-      <div className="w-full max-w-[371px] mt-[20px] h-[1px] bg-white"></div>
-      <div
-        onClick={() => login()}
-        className="w-full max-w-[371px] h-[38px] hover:border-[4px] hover:border-[#F8BB49] hover:cursor-pointer flex items-center justify-center bg-[#ffffff] rounded-[10px] shadow-[28px_28px_50px_0px_rgba(17,17,17,0.25)] mt-5 placeholder-gray-500 transform hover:scale-100"
-      >
-        <div className="bg-white w-[30px] h-[30px] rounded-full flex items-center justify-center mr-[10px]">
-          <img src={GoogleIcon} alt="Google" />
+      <div className="w-[90%] max-w-[600px] flex absolute flex-col justify-center items-center h-auto sm:h-[600px] bg-black/10 rounded-lg sm:rounded-full p-10 shadow-[inset_-4px_-4px_5px_0px_rgba(255,255,255,0.25),inset_4px_4px_5px_0px_rgba(255,255,255,0.25)] backdrop-blur-[30px]">
+        <img className="mx-auto mt-[-20px]" src={Logo} alt="Logo Bossa" />
+        <div className="w-full cursor-default max-w-[368px] h-[46px] text-white text-[32px] font-normal font-alatsi text-left">
+          Login
         </div>
-        <div className="text-[#000000] font-alatsi font-[500]">
-          Continuar com o Google
+        <LoginInput placeholder="E-mail"></LoginInput>
+        <LoginInput placeholder="Senha"></LoginInput>
+        <div className="w-full max-w-[368px] mt-[10px] h-[19px] text-right text-white text-base font-normal font-alatsi underline hover:cursor-pointer hover:opacity-70">
+          Esqueci minha senha!
+        </div>
+        <LoginButton></LoginButton>
+        <div className="w-full max-w-[371px] mt-[20px] h-[1px] bg-white"></div>
+        <div
+          onClick={() => login()}
+          className="w-full max-w-[371px] h-[38px] hover:border-[4px] hover:border-[#F8BB49] hover:cursor-pointer flex items-center justify-center bg-[#ffffff] rounded-[10px] shadow-[28px_28px_50px_0px_rgba(17,17,17,0.25)] mt-5 placeholder-gray-500 transform hover:scale-100"
+        >
+          <div className="bg-white w-[30px] h-[30px] rounded-full flex items-center justify-center mr-[10px]">
+            <img src={GoogleIcon} alt="Google" />
+          </div>
+          <div className="text-[#000000] font-alatsi font-[500]">
+            Continuar com o Google
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
