1aedb9d8afd7ba472c4a8a1842755ce434c5238f
Helena <helenavitoria1200@gmail.com>
2025-06-26 17:42:20 -0300

    feat: added CreateTeam screen

--- src/views/teams/CreateTeam.tsx
+++ src/views/teams/CreateTeam.tsx
@@ -5,6 +5,9 @@ import Box from "@/components/box/BoxContent";
 import InputTitle from "@/components/title/InputTitle";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Motion } from "@/components/animation/Motion";
+import InputString from "@/components/shared/InputString";
+import PlainButton from "@/components/shared/PlainButton";
+import ColoredButton from "@/components/shared/ColoredButton";
 
 const CreateTeam = () => {
   const navigate = useNavigate();
@@ -18,7 +21,7 @@ const CreateTeam = () => {
         <BackButton onClick={() => navigate(previousRoute)} />
         <PageTitle
           // icon client
-          icon="fa-solid fa-user-tie"
+          icon="fa-solid fa-circle-plus"
           marginTop="mt-4"
           title={`Cadastrar Equipe`}
         ></PageTitle>
@@ -30,101 +33,50 @@ const CreateTeam = () => {
             height="h-fit"
           >
             <InputTitle title="Equipe"></InputTitle>
-            {/* <div className="flex gap-6 flex-row w-full"> */}
-            {/* <InputString
-              title="NOME DO CLIENTE"
-              width="w-2/3"
-              isMandatory={true}
-              placeholder="Pesquise o cliente..."
-              height="h-[40px]"
-            />
-            <div className="mt-auto mb-3 w-1/3">
-              <PlainButton
-                title="NOVA EQUIPE"
-                color="bg-customYellow"
-                height="h-10"
-                width="w-full"
-              />
-            </div>
-          </div>
-          <div className="mt-6">
-            <InputTitle title="Serviço"></InputTitle>
-            <div className="flex gap-6 flex-row w-full">
-              <Select
-                options={[{ id: 1, name: "Banner" }]}
-                title="TIPO DE SERVIÇO"
-                isMandatory={true}
-                onChange={(selectedOption) => console.log(selectedOption)}
-                width="w-1/3"
-                height="h-[40px]"
-              />
-              <div className="flex items-center w-1/3">
-                <PlainButton
-                  title="NOVO SERVIÇO"
-                  color="bg-customYellow"
-                  height="h-10"
-                  width="w-full"
-                />
-              </div>
-              <div className="mt-auto mb-3 w-1/3">
-                <InputDate
-                  title="PRAZO"
-                  isMandatory={false}
-                  onChange={(date) => console.log(date)}
-                />
-              </div>
-            </div>
-            <TextArea
-              title="DESCRIÇÃO DO SERVIÇO"
-              placeholder="Digite detalhes sobre o serviço..."
-              isMandatory={true}
-              width="w-full"
-              height="h-[100px]"
-              rounded="rounded-[20px]"
-            />
-            <div className="flex flex-row gap-6">
+            <div className="w-full flex flex-col">
               <InputString
-                title="LINK DO DRIVE"
-                width="w-2/3"
+                title="NOME DA EQUIPE"
+                width="w-full"
                 isMandatory={true}
-                placeholder="Insira o link para o Drive do serviço..."
+                placeholder="Pesquise o cliente..."
                 height="h-[40px]"
               />
-              <InputQuantity
-                title="QUANTIDADE"
-                height="h-[40px]"
-                width="w-1/3"
-                isMandatory={true}
-                value={1}
-                min={1}
-                max={100}
-                onChange={(value) => console.log(value)}
-              />
+              <div className="flex gap-6 flex-row">
+                <InputString
+                  title="NOME DO SETOR"
+                  width="w-2/3"
+                  isMandatory={true}
+                  placeholder="Pesquise o cliente..."
+                  height="h-[40px]"
+                />
+                <div className="mt-auto mb-3 w-1/3">
+                  <PlainButton
+                    title="NOVO SETOR"
+                    color="bg-customYellow"
+                    height="h-10"
+                    width="w-full"
+                  />
+                </div>
+              </div>
+              <div className="flex w-full mt-10 justify-center">
+              <ColoredButton
+                onClick={() => {
+                  // navigate("/reports");
+                }}
+                title="SALVAR EQUIPE"
+                width="w-[40%]"
+                icon={"fa-solid fa-floppy-disk"}
+                color="customYellow"
+                justify="justify-center"
+              ></ColoredButton>
             </div>
-          </div>
-          <div className="mt-6">
-            <InputTitle title="Atribuição"></InputTitle>
-            <Select
-              options={[{ id: 1, name: "Design" }]}
-              title="SETOR RESPONSÁVEL"
-              isMandatory={true}
-              onChange={(selectedOption) => console.log(selectedOption)}
-              width="w-1/3"
-              height="h-[40px]"
-            />
-          </div>
-          <div className="flex w-full mt-10 justify-center">
-            <ColoredButton
-              onClick={() => {
-                // navigate("/reports");
-              }}
-              title="SALVAR DEMANDA"
-              width="w-[40%]"
-              icon={"fa-solid fa-floppy-disk"}
-              color="customYellow"
-              justify="justify-center"
-            ></ColoredButton>
-          </div> */}
+            </div>
+
+
+
+
+
+
           </Box>
         </Motion>
       </BaseScreen>
