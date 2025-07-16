5af858b6598f53196df4443a98d9c0d3f99e35e9
Helena <helenavitoria1200@gmail.com>
2025-07-16 17:21:31 -0300

    feat: added clientSchema using Zod

---
+++ src/schemas/clientSchema.ts
@@ -0,0 +1,77 @@
+import { validateInput } from '@/utils/validateInput';
+import { z } from 'zod';
+
+const clientSchema = z.object({
+  businessId: z
+   .number()
+    .min(1, "Selecione um negócio")
+    .optional(),
+   enterpriseName: z
+     .string()
+        .min(1, "Nome da empresa deve ter pelo menos 1 caractere")
+        .max(255, "Nome da empresa não pode exceder 255 caracteres")
+        .regex(/^[A-Za-zÀ-ÖØ-öø-ÿ0-9\s\-&.,]+$/, "O nome da empresa contém caracteres inválidos."),
+    contactName: z
+        .string()
+        .min(1, "Nome do contato deve ter pelo menos 1 caractere")
+        .max(255, "Nome do contato não pode exceder 255 caracteres")
+        .regex(/^[A-Za-zÀ-ÖØ-öø-ÿ0-9\s\-&.,]+$/, "O nome do contato contém caracteres inválidos."),
+    contactEmail: z
+        .string()
+        .min(1, "Email do contato deve ter pelo menos 1 caractere")
+        .max(100, "Email do contato não pode exceder 100 caracteres")
+        .email("Email do contato deve ser um email válido")
+        .optional(),
+    contactPhone: z
+    .string()
+        .min(11, "Telefone deve ter 11 caracteres")
+        .max(11, "Telefone deve ter 11 caracteres")
+        .refine((val) => !val || validateInput(val, "phone"), "Telefone inválido")
+        .optional(),
+    entryDate: z
+        .string()
+        .optional()
+        .refine(
+            (val) => !val || validateInput(val, "entryDate"),
+            "Data de entrada inválida"
+        )
+        .optional(),
+
+    // verificar regra de negócio para o campo abaixo
+    contractEndDate: z
+        .string()
+        .refine(
+            (val) => !val || validateInput(val, "contractEndDate"),
+            "Data de término de contrato inválida"
+        )   
+        .optional(),
+    contractDescription: z
+        .string()
+        .min(10, "Descrição do contrato deve ter pelo menos 10 caracteres")
+        .max(500, "Descrição do contrato não pode exceder 500 caracteres")
+        .regex(/^[A-Za-zÀ-ÖØ-öø-ÿ0-9\s\-&.,]+$/, "A descrição do contrato contém caracteres inválidos.")
+        .optional(),
+    briefing: z
+        .string()
+        .min(10, "Briefing deve ter pelo menos 10 caracteres")
+        .max(1000, "Briefing não pode exceder 1000 caracteres")
+        .regex(/^[A-Za-zÀ-ÖØ-öø-ÿ0-9\s\-&.,]+$/, "O briefing contém caracteres inválidos.")
+        .optional(),
+    isActive: z
+        .boolean()
+        .optional()
+        .default(true)
+        .refine((val) => typeof val === 'boolean', "O campo 'Ativo' deve ser um booleano"),
+    classification: z
+        .string()
+        .min(1, "Classificação deve ter pelo menos 1 caractere")
+        .max(100, "Classificação não pode exceder 100 caracteres")
+        .regex(/^[A-Za-zÀ-ÖØ-öø-ÿ0-9\s\-&.,]+$/, "A classificação contém caracteres inválidos.")
+        .optional(),
+
+});
+
+type ClientFormData = z.infer<typeof clientSchema>;
+
+export { clientSchema };
+export type { ClientFormData };
