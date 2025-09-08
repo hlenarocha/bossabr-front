579aeb898eb5e951c6d6e08a390b3094139ce2db
hlenarocha <helenavitoria1200@gmail.com>
2025-09-08 12:42:18 -0300

    feat: add funcional CreateActivityModal in Workspace and Demands screens

--- src/components/task/TaskColumn.tsx
+++ src/components/task/TaskColumn.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import DropArea from "./DropArea";
 import TaskCard, { TaskCardProps } from "./TaskCard";
 
 interface Task {
@@ -14,19 +12,12 @@ interface TaskColumnProps {
   title: string;
   tasks: TaskCardProps[];
   status: string;
-  //setActiveCard: React.Dispatch<React.SetStateAction<number | null>>;
-  //activeCard: number | null;
-  //onDrop: (status: string, position: number) => void;
   onCardActionClick?: (event: React.MouseEvent, task: Task) => void;
 
 }
 
 const TaskColumn = (props: TaskColumnProps) => {
-  // const [activeDropIndex, setActiveDropIndex] = useState<number | null>(null);
-  // const [isDraggingOverColumn, setIsDraggingOverColumn] = useState(false);
-
   const filteredTasks = props.tasks.filter(task => task.status === props.status);
-  const isEmpty = filteredTasks.length === 0;
 
   const statusColor = {
     "não iniciada": "bg-gray-500",
@@ -37,30 +28,9 @@ const TaskColumn = (props: TaskColumnProps) => {
 
   };
 
-  // const handleDragOver = (e: React.DragEvent, index: number) => {
-  //   e.preventDefault();
-  //   setActiveDropIndex(index);
-  // };
-
-  // const handleColumnDragOver = (e: React.DragEvent) => {
-  //   e.preventDefault();
-  //   setIsDraggingOverColumn(true);
-  //   if (isEmpty) {
-  //     setActiveDropIndex(0); // Força mostrar o drop area em colunas vazias
-  //   } else if (activeDropIndex === null) {
-  //     setActiveDropIndex(-1); // Mostra área no topo
-  //   }
-  // };
-
-  // const handleDragLeave = () => {
-  //   setIsDraggingOverColumn(false);
-  //   setActiveDropIndex(null);
-  // };
-
   return (
     <div
       className="flex flex-col w-full h-[250px]"
-      // onDragLeave={handleDragLeave}
     >
       <div className="flex bg-zinc-900 p-2 rounded-md items-center just gap-2 mb-4 px-4">
         <span className={`w-3 h-3 rounded-full ${statusColor[props.status as keyof typeof statusColor]}`} />
@@ -71,7 +41,7 @@ const TaskColumn = (props: TaskColumnProps) => {
       >
       
 
-        {filteredTasks.map((task, index) => (
+        {filteredTasks.map((task) => (
           <div
             key={task.indexCard}
             className="relative"
